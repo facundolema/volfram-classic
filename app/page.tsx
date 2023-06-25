ca0ca68fe4d2b8adcc717538feb8de8b89552ed9
FacundoLema <facurlema@gmail.com>
2023-06-25 01:41:11 -0300

    Revert "added"
    This reverts commit e1f310b7a771928d81a434a7b9fc0b21c70ba4d3.

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,11 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen font-bold text-gray-800
+    <main className="flex flex-col bg-green-100 h-screen font-bold text-gray-800
         lg:w-1/2 sm:w-full m-auto px-10 justify-center">
         <NavBar/>
         <Title/>
         <AppPreview/>
-        <Details/>
     </main>
   )
 }
@@ -20,7 +19,7 @@ function AppIcon (
 {
   return (
     <div className='flex flex-col justify-center items-center gap-2'>
-      <div className={`h-20 w-20 bg-[#DDD] rounded-[1rem]`}/>
+      <div className={`h-20 w-20 bg-[${color}] rounded-[1rem]`}/>
       <div className=''>{alt}</div>
     </div>
   )
@@ -55,7 +54,7 @@ function NavBar ()
 function Title ()
 {
   return (
-    <section className='flex flex-col justify-self-center'>
+    <section className='flex flex-col bg-red-300 justify-self-center'>
       <div className="flex items-center justify-center font-bold text-8xl">
         Volfram Studio
       </div>
@@ -69,34 +68,12 @@ function Title ()
 function AppPreview ()
 {
   return (
-    <section className='flex flex-wrap justify-center gap-16'>
+    <section className='flex flex-wrap justify-center gap-16 bg-blue-200'>
       <AppIcon src='' alt='Kodkod' color='#FFE49D'/>
       <AppIcon src='' alt='Humpback' color='#9DCAFF'/>
       <AppIcon src='' alt='Crow' color='#404040'/>
       <AppIcon src='' alt='Buho' color='#B5CDAD'/>
       <AppIcon src='' alt='Longshot' color='#FF8787'/>
-    </section>
-  )
-}
-
-function Details()
-{
-  return (
-    <section className='rounded-lg bg-gray-200 p-10'>
-      <div className='border border-gray-400 flex items-center'>
-          <div className={`h-20 w-20 bg-[#DDD] rounded-[1rem]`}/>
-          <div className='flex flex-col h-20 bg-green-200 p-2'>
-            <h1 className='justify-self-start'>Kodkod</h1>
-            <h1 className='justify-self-start font-thin text-sm'>v0.0.3 (alpha)</h1>
-          </div>
-          <div className='flex flex-col font-thin text-sm'>
-            <p>
-            Cross platform note taking app with markdown support, code highlighting,
-            custom fonts, custom background colors, cloud sync, plug-in support, drawing,
-            stylus support, and more. 
-            </p>
-          </div>
-      </div>
     </section>
   )
 }
